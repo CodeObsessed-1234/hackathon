73164281c500871fc9d3527ba4fa13070056c1c3
CodeObsessed-1234 <86585422+CodeObsessed-1234@users.noreply.github.com>
2022-05-20 14:29:25 +0530

    Update MainPhoneBook.c

--- MainPhoneBook.c
+++ MainPhoneBook.c
@@ -2,6 +2,7 @@
 #include "./Address.h"
 #include "./City.h"
 #include "./EmialId.h"
+#include "./Name.h"
 #include <stdio.h>
 #include<conio.h>
 
@@ -11,11 +12,16 @@ struct MainPhoneBook
     char city[20];
     char address[50];
     char phoneNo[12];
+    char email[20]
 };
 
 
+typedef struct MainPhoneBook mpb;
+
 void main(){
     int choice;
+    mpb * r;
+    r=(mpb *)malloc(sizeof(mpb));
     printf("Welco;me to your Phone Book- ");
     printf("Press 1 for Create New Contact\n");
     printf("Press 2 to see all Contact\n");
@@ -29,7 +35,22 @@ void main(){
         switch (choice)
         {
         case 1:
-            /* code */
+           puts("Enter Name ");
+           scanf("%s",&r->name);
+           puts("Enter City ");
+           scanf("%s",&r->city);
+           puts("Enter Address ");
+           scanf("%s",&r->address);
+           puts("Enter Phone No. ");
+           scanf("%s",&r->phoneNo);
+           puts("Enter Email ");
+           scanf("%s",&r->email);
+           
+           createNameFile(r->name);
+           createAddressFile(r->address);
+           createCityFile(r->city);
+           createEmailId(r->email);
+           createPhoneNoFile(r->phoneNo);
             break;
         
         default:
